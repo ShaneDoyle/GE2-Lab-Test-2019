9b5ff8ee9963165b067a04219f5fe821470488f0
ShaneDoyle <shanedoyle21@hotmail.com>
2019-03-26 09:46:54 +0000

    Commit 1
    Added colours to each fighter when spawned.

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -8,9 +8,14 @@ public class Base : MonoBehaviour
     public float tiberium = 0;
 
     public TextMeshPro text;
-
     public GameObject fighterPrefab;
 
+
+    //Private varaibles.
+    private GameObject Fighter;
+
+    private Renderer rend;
+    private Color colorStart;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +23,38 @@ public class Base : MonoBehaviour
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+            colorStart = r.material.color;
+            Debug.Log("Renderer complete");
         }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "" + tiberium;
+
+        if(tiberium == 10)
+        {
+
+            StartCoroutine("SpawnFighter");
+        }
+
+        StartCoroutine("IncreaseTiberium");
+    }
+
+
+    IEnumerator SpawnFighter()
+    {
+        //Wait 1 second.
+        yield return new WaitForSeconds(1);
+        Fighter = GameObject.Instantiate<GameObject>(fighterPrefab);
+        Fighter.transform.position = transform.position; //Spawn at base.
+
+        Fighter script = Fighter.GetComponent<Fighter>();
+        script.Base = this.gameObject;
     }
 }
+
+
---
+++ Assets/Fighter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+
+    public GameObject Base;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+
+            Material m_Material = Base.GetComponent<Renderer>().material;
+            r.material = m_Material;
+           // r.material.color = Base.GetComponent<Color>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
